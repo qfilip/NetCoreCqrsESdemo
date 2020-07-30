7f37843de19c0ca3aac50e74a463c69fa8f55638
qfilip <kukolj.filip@gmail.com>
2020-07-30 16:59:22 +0200

    CreateCocktailCommand - WIP

---
+++ NetCoreCqrsESdemo.BusinessLogic/Commands/CocktailCommands/CreateCocktailCommand.cs
@@ -0,0 +1,42 @@
+﻿using NetCoreCQRSdemo.Domain.Dtos;
+using NetCoreCQRSdemo.Domain.Enumerations;
+using NetCoreCQRSdemo.Persistence.Context;
+using NetCoreCqrsESdemo.BusinessLogic.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetCoreCqrsESdemo.BusinessLogic.Commands.CocktailCommands
+{
+    public class CreateCocktailCommand : BaseCommand<CocktailDto>
+    {
+        public CreateCocktailCommand(CocktailDto dto) : base (dto, eCommandType.Create) { }
+
+        public class Handler : BaseHandler<CreateCocktailCommand, CocktailDto>
+        {
+            public Handler(ApplicationDbContext dbContext) : base(dbContext) { }
+
+            public async override Task<CocktailDto> Handle(CreateCocktailCommand request, CancellationToken cancellationToken)
+            {
+                var entityId = Guid.NewGuid().ToString();
+
+                var entity = _appMapper.ToEntity(request._dto);
+                entity.Id = entityId;
+
+                foreach (var excerpt in entity.Excerpts)
+                {
+                    excerpt.Id = Guid.NewGuid().ToString();
+                    excerpt.CocktailId = entityId;
+                }
+
+                await _dbContext.AddAsync(entity);
+                var result = _appMapper.ToDto(entity);
+
+
+                return result;
+            }
+        }
+    }
+}
